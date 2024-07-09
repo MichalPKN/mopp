d092697f838e54351515c3afe3cb92a80baef158
MichalPKN <michal.pokorny24@gmail.com>
2024-07-09 17:50:07 +0200

    git refresh

--- components/stats_render.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-class renderer:
-    def __init__(self):
-        pass
-    
-    def render(self, data):
-        return render_template('index.html', data=data)
