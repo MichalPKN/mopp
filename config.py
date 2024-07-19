42ac2cbc0003a3e9c97176785919b139b35b552c
MichalPKN <michal.pokorny24@gmail.com>
2024-07-19 11:43:02 +0200

    config update

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 #generate your own API key from https://developer.riotgames.com/
-api_key = "RGAPI-4a37aace-6d1a-4ef2-a529-68d316d98d07"
+api_key = "RGAPI-3df99d99-937d-4b34-b195-c72b3e21fab2"
 
-#kesha's account, should probably work if replaced with another account (adds it to database, loading matches might take a while)
+#kesha's account, should probably work if replaced with another account (adds it to database, loading matches might take a few minutes at first)
 region = "euw1"
 riot_id = "erectwillump"
 tag_line = "boing"
 
 #no need to change
 continent = "europe"
+
